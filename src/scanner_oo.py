4ea7d4faa78a4f9ec157193ab86d87af524d098f
Marcin Szubert <54717847+marszub@users.noreply.github.com>
2021-10-15 17:47:25 +0200

    reserved tokens

--- src/scanner_oo.py
+++ src/scanner_oo.py
@@ -7,6 +7,16 @@ tokens = [
     'DIVISION'
 ]
 
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MAT_PLUS = r'\.\+'
+t_MAT_MINUS = r'\.-'
+t_MAT_TIMES = r'\.\*'
+t_MAT_DIVIDE = r'\./'
+t_ASSIGN = r'='
+t_PLUS_ASSIGN = r'\+='
 
 
 def Scanner():
