ee23653cd5ea8c03671575350d5df5a23a38699b
JanuszJakubiec <54719269+JanuszJakubiec@users.noreply.github.com>
2022-01-02 22:50:01 +0100

    Adding matrix type and size checking

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 
     if mode == 'type_check':
         try:
-            filename = sys.argv[1] if len(sys.argv) > 1 else "example10.m"
+            filename = sys.argv[1] if len(sys.argv) > 1 else "example8.m"
             file = open(filename, "r")
         except IOError:
             print("Cannot open {0} file".format(filename))
--- src/type_checker.py
+++ src/type_checker.py
@@ -17,22 +17,81 @@ def add_binary_type(operator, left, right, result):
         return
     ttype[operator][left][right] = result
 
+def is_matrix_type(type):
+    if type == "matrix_int" or type == "matrix_float":
+        return True
+    return False
+
+def check_columns(lsize_columns, rsize_columns):
+    if lsize_columns == rsize_columns:
+        return lsize_columns, True
+    if lsize_columns == -1:
+        return rsize_columns, True
+    if rsize_columns == -1:
+        return lsize_columns, True
+    return None, False
+
+def check_size_compatibility_and_return_type(lsize_row, lsize_columns, rsize_row, rsize_columns, type):
+    if lsize_row == rsize_row:
+        value, check_successful = check_columns(lsize_columns, rsize_columns)
+        if check_successful:
+            return type + "/" + str(lsize_row) + "/" + str(value)
+        else:
+            return None
+    if lsize_row == -1:
+        value, check_successful = check_columns(lsize_columns, rsize_columns)
+        if check_successful:
+            return type + "/" + str(lsize_row) + "/" + str(value)
+        else:
+            return None
+    if rsize_row == -1:
+        value, check_successful = check_columns(lsize_columns, rsize_columns)
+        if check_successful:
+            return type + "/" + str(lsize_row) + "/" + str(value)
+        else:
+            return None
+    return None
+
+
 def get_binary_type(operator, left, right):
-    lsize = -1
-    rsize = -1
+    lsize_row = -2
+    rsize_row = -2
+    lsize_columns = -2
+    rsize_columns = -2
     if "/" in left:
         tab = left.split("/")
-        lsize = int(tab[1],10)
+        lsize_row = int(tab[1],10)
+        lsize_columns = int(tab[2],10)
         left = tab[0]
     if "/" in right:
         tab = right.split("/")
-        rsize = int(tab[1], 10)
+        rsize_row = int(tab[1], 10)
+        rsize_columns = int(tab[2], 10)
         right = tab[0]
-    if lsize != rsize:
-        return None
     if left == "error" or right == "error":
         return "error"
-    return ttype.get(operator, {}).get(left, {}).get(right, None)
+
+    type_and_size_check = ttype.get(operator, {}).get(left, {}).get(right, None)
+    if type_and_size_check == None:
+        return None
+
+    type = type_and_size_check[0]
+    size_check = type_and_size_check[1]
+    if size_check:
+        if operator == "*":
+            if lsize_columns == rsize_row or lsize_columns == -1 or rsize_row == -1:
+                return type + "/" + str(lsize_row) + "/" + str(rsize_columns)
+            else:
+                return None
+        else:
+            return check_size_compatibility_and_return_type(lsize_row, lsize_columns, rsize_row, rsize_columns, type)
+
+    if is_matrix_type(left):
+        return type + "/" + str(lsize_row) + "/" + str(lsize_columns)
+    if is_matrix_type(right):
+        return type + "/" + str(rsize_row) + "/" + str(rsize_columns)
+
+    return type
 
 def convert_operator(operator):
     if operator == "+=":
@@ -49,9 +108,6 @@ def convert(type):
     if type == "int":
         return "float"
     return type
-
-#def matrix_type_decide(rtype, ltype, node, table):
-
 
 class NodeVisitor(object):
 
@@ -119,9 +175,9 @@ class TypeChecker(NodeVisitor):
             return "error"
         if node.operator == "==":
             return "int"
-        if ltype == rtype or convert(ltype) == convert(rtype):
+        if (ltype == rtype or convert(ltype) == convert(rtype)) and "/" not in ltype:
             return "int"
-        print("Line " + str(node.position) + ": Comparing types mismatch. Types: " + ltype + " " + rtype)
+        print("Line " + str(node.position) + ": Illegal compare of types: " + ltype + " " + rtype)
         return "error"
 
     def visit_BinaryOperator(self, node):
@@ -153,12 +209,31 @@ class TypeChecker(NodeVisitor):
 
     def visit_Assignment(self, node):
         rtype = self.visit(node.right)
+        if rtype == "error":
+            return "error"
         if node.operator == "=":
             if node.left.__class__.__name__ == "Id":
                 self.symbol_table.put(node.left.value, rtype)
+                return None
             else:
-                print("unimplemented")
-            return None
+                if node.left.__class__.__name__ == "MatrixCall":
+                    ltype = self.visit(node.left)
+                    if ltype == "error":
+                        return "error"
+                    if node.left.expression.__class__.__name__ == "Id":
+                        type = self.symbol_table.get(node.left.expression.value)
+                        rows = -2
+                        columns = -2
+                        if "/" in type:
+                            tab = type.split("/")
+                            rows = tab[1]
+                            columns = tab[2]
+                            type = tab[0]
+                        if type == "matrix_int" and rtype == "float":
+                            type = self.symbol_table.put(node.left.expression.value, type + "/" + rows + "/" + columns)
+                            return None
+            print("Line " + str(node.position) + ": Cannot assign value to something that is not variable")
+            return "error"
         if node.left.__class__.__name__ == "Id":
             ltype = self.visit(node.left)
             returnType = get_binary_type(convert_operator(node.operator), ltype, rtype)
@@ -167,12 +242,58 @@ class TypeChecker(NodeVisitor):
                 return 'error'
             if returnType != "error":
                 self.symbol_table.put(node.left.value, rtype)
-        else:
-            print("unimplemented")
-        return None
+        if node.left.__class__.__name__ == "MatrixCall":
+            ltype = self.visit(node.left)
+            if ltype == "error":
+                return "error"
+            if node.left.expression.__class__.__name__ == "Id":
+                rtype = get_binary_type(convert_operator(node.operator), ltype, rtype)
+                type = self.symbol_table.get(node.left.expression.value)
+                rows = -2
+                columns = -2
+                if "/" in type:
+                    tab = type.split("/")
+                    rows = tab[1]
+                    columns = tab[2]
+                    type = tab[0]
+                if type == "matrix_int" and rtype == "float":
+                    self.symbol_table.put(node.left.expression.value, type + "/" + rows + "/" + columns)
+                    return None
+        print("Line " + str(node.position) + ": Cannot assign value to something that is not variable")
+        return "error"
 
     def visit_MatrixCall(self, node):
-        pass
+        type = self.visit(node.expression)
+        xtype = self.visit(node.x)
+        ytype = self.visit(node.y)
+        rows = -2
+        columns = -2
+        if "/" in type:
+            tab = type.split("/")
+            rows = int(tab[1], 10)
+            columns = int(tab[2], 10)
+            type = tab[0]
+        if not is_matrix_type(type):
+            print("Line " + str(node.position) + ": Type: " + type + " is not Matrix type!")
+            return 'error'
+        if xtype != "int":
+            print("Line " + str(node.x.position) + ": Illegal Matrix call type: " + xtype + " it should be integer!")
+            return 'error'
+        if ytype != "int":
+            print("Line " + str(node.y.position) + ": Illegal Matrix call type: " + ytype + " it should be integer!")
+            return 'error'
+        if node.x.__class__.__name__ == "Integer":
+            if not (node.x.value >= 0 and (node.x.value < rows or rows == -1)):
+                print("Line " + str(node.x.position) + ": Matrix index x out of range!")
+                return "error"
+        if node.y.__class__.__name__ == "Integer":
+            if not (node.y.value >= 0 and (node.y.value < columns or columns == -1)):
+                print("Line " + str(node.y.position) + ": Matrix index y out of range!")
+                return "error"
+        if type == "matrix_int":
+            return "int"
+        else:
+            return "float"
 
     def visit_Return(self, node):
         self.visit(node.expression)
@@ -197,8 +318,10 @@ class TypeChecker(NodeVisitor):
             element_type = element_type.split("/")
             row_size = int(element_type[1], 10)
             element_type = element_type[0]
+            if size == None:
+                size = row_size
             if row_size != size:
-                print("Line " + str(node.position) + ": Matrix rows have different sizes")
+                print("Line " + str(row.position) + ": Matrix rows have different sizes")
                 type == "error"
             if element_type == "row_float":
                 type = "row_float"
@@ -208,8 +331,8 @@ class TypeChecker(NodeVisitor):
         if type == "error":
             return "error"
         if type == "row_int":
-            return ("matrix_int" + "/" + str(size))
-        return ("matrix_float" + "/" + str(size))
+            return "matrix_int" + "/" + str(len(node.rows)) + "/" + str(size)
+        return "matrix_float" + "/" + str(len(node.rows)) + "/" + str(size)
 
     def visit_Row(self, node):
         type = "int"
@@ -223,8 +346,8 @@ class TypeChecker(NodeVisitor):
         if type == "error":
             return "error"
         if type == "int":
-            return ("row_int" + "/" + str(len(node.sequence)))
-        return ("row_float" + "/" + str(len(node.sequence)))
+            return ("row_int" + "/" + str(len(node.sequence)) + "/" + str(1))
+        return ("row_float" + "/" + str(len(node.sequence)) + "/" + str(1))
 
 
 
@@ -259,9 +382,9 @@ class TypeChecker(NodeVisitor):
         type = self.visit(node.expression)
         if type == "int":
             if node.expression.__class__.__name__ == "Integer":
-                return ("matrix_int" + "/" + str(node.expression.value))
+                return "matrix_int" + "/" + str(node.expression.value) + "/" + str(node.expression.value)
             else:
-                return ("matrix_int" + "/-1")
+                return "matrix_int/-1/-1"
         else:
             print("Line " + str(node.position) + ": Illegal eye function call. Argument type cannot be " + type)
             return "error"
@@ -270,9 +393,9 @@ class TypeChecker(NodeVisitor):
         type = self.visit(node.expression)
         if type == "int":
             if node.expression.__class__.__name__ == "Integer":
-                return ("matrix_int" + "/" + str(node.expression.value))
+                return "matrix_int" + "/" + str(node.expression.value) + "/" + str(node.expression.value)
             else:
-                return ("matrix_int" + "/-1")
+                return "matrix_int/-1/-1"
         else:
             print("Line " + str(node.position) + ": Illegal zeros function call. Argument type cannot be " + type)
             return "error"
@@ -281,52 +404,73 @@ class TypeChecker(NodeVisitor):
         type = self.visit(node.expression)
         if type == "int":
             if node.expression.__class__.__name__ == "Integer":
-                return ("matrix_int" + "/" + str(node.expression.value))
+                return "matrix_int" + "/" + str(node.expression.value) + "/" + str(node.expression.value)
             else:
-                return ("matrix_int" + "/-1")
+                return "matrix_int/-1/-1"
         else:
             print("Line " + str(node.position) + ": Illegal ones function call. Argument type cannot be " + type)
             return "error"
 
-add_binary_type("+", "int", "int", "int")
-add_binary_type("+", "float", "int", "float")
-add_binary_type("+", "int", "float", "float")
-add_binary_type("+", "float", "float", "int")
-add_binary_type("+", "string", "string", "float")
+add_binary_type("+", "int", "int", ("int", False))
+add_binary_type("+", "float", "int", ("float", False))
+add_binary_type("+", "int", "float", ("float", False))
+add_binary_type("+", "float", "float", ("int", False))
+add_binary_type("+", "string", "string", ("float", False))
+add_binary_type("+", "matrix_int", "matrix_float", ("matrix_float", True))
+add_binary_type("+", "matrix_int", "matrix_int", ("matrix_int", True))
+add_binary_type("+", "matrix_float", "matrix_int", ("matrix_float", True))
+add_binary_type("+", "matrix_float", "matrix_float", ("matrix_float", True))
 
-add_binary_type("-", "int", "int", "int")
-add_binary_type("-", "float", "int", "float")
-add_binary_type("-", "int", "float", "float")
-add_binary_type("-", "float", "float", "float")
-add_binary_type("-", "string", "string", "string")
+add_binary_type("-", "int", "int", ("int", False))
+add_binary_type("-", "float", "int", ("float", False))
+add_binary_type("-", "int", "float", ("float", False))
+add_binary_type("-", "float", "float", ("int", False))
+add_binary_type("-", "string", "string", ("float", False))
+add_binary_type("-", "matrix_int", "matrix_float", ("matrix_float", True))
+add_binary_type("-", "matrix_int", "matrix_int", ("matrix_int", True))
+add_binary_type("-", "matrix_float", "matrix_int", ("matrix_float", True))
+add_binary_type("-", "matrix_float", "matrix_float", ("matrix_float", True))
 
-add_binary_type("*", "int", "int", "int")
-add_binary_type("*", "float", "int", "float")
-add_binary_type("*", "int", "float", "float")
-add_binary_type("*", "float", "float", "float")
-add_binary_type("*", "string", "string", "string")
+add_binary_type("*", "int", "int", ("int", False))
+add_binary_type("*", "float", "int", ("float", False))
+add_binary_type("*", "int", "float", ("float", False))
+add_binary_type("*", "float", "float", ("int", False))
+add_binary_type("*", "string", "int", ("string", False))
+add_binary_type("*", "int", "string", ("string", False))
+add_binary_type("*", "matrix_int", "matrix_float", ("matrix_float", True))
+add_binary_type("*", "matrix_int", "matrix_int", ("matrix_int", True))
+add_binary_type("*", "matrix_float", "matrix_int", ("matrix_float", True))
+add_binary_type("*", "matrix_float", "matrix_float", ("matrix_float", True))
 
-add_binary_type("/", "int", "int", "float")
-add_binary_type("/", "float", "int", "float")
-add_binary_type("/", "int", "float", "float")
-add_binary_type("/", "float", "float", "float")
+add_binary_type("/", "int", "int", ("float", False))
+add_binary_type("/", "float", "int", ("float", False))
+add_binary_type("/", "int", "float", ("float", False))
+add_binary_type("/", "float", "float", ("int", False))
 
-add_binary_type(".+", "matrix_int", "matrix_int", "matrix_int")
-add_binary_type(".+", "matrix_float", "matrix_int", "matrix_float")
-add_binary_type(".+", "matrix_int", "matrix_float", "matrix_float")
-add_binary_type(".+", "matrix_float", "matrix_float", "matrix_float")
+add_binary_type(".+", "matrix_int", "int", ("matrix_int", False))
+add_binary_type(".+", "matrix_int", "float", ("matrix_float", False))
+add_binary_type(".+", "matrix_float", "int", ("matrix_float", False))
+add_binary_type(".+", "matrix_float", "float", ("matrix_float", False))
+add_binary_type(".+", "int", "matrix_int", ("matrix_int", False))
+add_binary_type(".+", "float", "matrix_int", ("matrix_float", False))
+add_binary_type(".+", "int", "matrix_float", ("matrix_float", False))
+add_binary_type(".+", "float", "matrix_float", ("matrix_float", False))
 
-add_binary_type(".-", "matrix_int", "matrix_int", "matrix_int")
-add_binary_type(".-", "matrix_float", "matrix_int", "matrix_float")
-add_binary_type(".-", "matrix_int", "matrix_float", "matrix_float")
-add_binary_type(".-", "matrix_float", "matrix_float", "matrix_float")
+add_binary_type(".-", "matrix_int", "int", ("matrix_int", False))
+add_binary_type(".-", "matrix_int", "float", ("matrix_float", False))
+add_binary_type(".-", "matrix_float", "int", ("matrix_float", False))
+add_binary_type(".-", "matrix_float", "float", ("matrix_float", False))
 
-add_binary_type(".*", "matrix_int", "matrix_int", "matrix_int")
-add_binary_type(".*", "matrix_float", "matrix_int", "matrix_float")
-add_binary_type(".*", "matrix_int", "matrix_float", "matrix_float")
-add_binary_type(".*", "matrix_float", "matrix_float", "matrix_float")
+add_binary_type(".*", "matrix_int", "int", ("matrix_int", False))
+add_binary_type(".*", "matrix_int", "float", ("matrix_float", False))
+add_binary_type(".*", "matrix_float", "int", ("matrix_float", False))
+add_binary_type(".*", "matrix_float", "float", ("matrix_float", False))
+add_binary_type(".*", "int", "matrix_int", ("matrix_int", False))
+add_binary_type(".*", "float", "matrix_int", ("matrix_float", False))
+add_binary_type(".*", "int", "matrix_float", ("matrix_float", False))
+add_binary_type(".*", "float", "matrix_float", ("matrix_float", False))
 
-add_binary_type("./", "matrix_int", "matrix_int", "matrix_float")
-add_binary_type("./", "matrix_float", "matrix_int", "matrix_float")
-add_binary_type("./", "matrix_int", "matrix_float", "matrix_float")
-add_binary_type("./", "matrix_float", "matrix_float", "matrix_float")
+add_binary_type("./", "matrix_int", "int", ("matrix_float", False))
+add_binary_type("./", "matrix_int", "float", ("matrix_float", False))
+add_binary_type("./", "matrix_float", "int", ("matrix_float", False))
+add_binary_type("./", "matrix_float", "float", ("matrix_float", False))
